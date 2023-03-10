deede7d5d907d93bdd12b87d59b734c8d71f1eef
Zac Luchette <luchettezac@gmail.com>
2023-03-09 22:06:21 -0500

    Header created

---
+++ Source - Discworld Homebrew [by InnocentGoblin].js
@@ -0,0 +1,15 @@
+/*	-WHAT IS THIS?-
+	This file adds optional material to "MPMB's Character Record Sheet" found at https://flapkan.com/mpmb/charsheets
+	Import this file using the "Add Extra Materials" bookmark.
+	-KEEP IN MIND-
+	It is recommended to enter the code in a fresh sheet before adding any other information (i.e. before making your character with it).
+*/
+
+/*	-INFORMATION-
+	Subject:	Source
+	Effect:		This script adds a Source to the MPMB character sheet sheet for the Discworld 5e D&D homebrew, for organizational and search purposes within the sheet.
+				The homebrew pdf upon which this Source is based was created by InnocentGoblin and is inspired by Discworld and the mind of Sir Terry Pratchett.			
+				It can be found here (https://www.gmbinder.com/share/-MzzKRfU9Tvu2xCsJiRS) 
+	Code by:	OtGW (Zac Luchette) https://github.com/OtGW
+	Date:		2023-03-09 (sheet v13.1.4+221130)
+*/
