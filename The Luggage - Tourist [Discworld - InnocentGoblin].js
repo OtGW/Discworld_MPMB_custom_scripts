0273071bf45d2270f358067849ac3a8aace71f2b
Zac Luchette <luchettezac@gmail.com>
2023-03-10 10:34:04 -0500

    File+header for The Luggage

---
+++ The Luggage - Tourist [Discworld - InnocentGoblin].js
@@ -0,0 +1,15 @@
+/*	-WHAT IS THIS?-
+	This file adds optional material to "MPMB's Character Record Sheet" found at https://flapkan.com/mpmb/charsheets
+	Import this file using the "Add Extra Materials" bookmark.
+	-KEEP IN MIND-
+	It is recommended to enter the code in a fresh sheet before adding any other information (i.e. before making your character with it).
+*/
+
+/*	-INFORMATION-
+	Subject:	Creature
+	Effect:		This script adds a creature called "The Luggage" for the Ranger subclass "The Tourist".
+				The homebrew pdf upon which this race is based was created by InnocentGoblin and is inspired by Discworld and the mind of Sir Terry Pratchett.			
+				The pdf can be found here (https://www.gmbinder.com/share/-MzzKRfU9Tvu2xCsJiRS)
+	Code by:	OtGW (Zac Luchette) https://github.com/OtGW
+	Date:		2023-03-10 (sheet v13.1.4+221130)
+*/
